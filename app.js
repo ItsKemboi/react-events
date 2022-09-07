a1d3fcd8fcf0dc5257aaa524c73bd152f6c5a338
React Dev <113049933+ItsKemboi@users.noreply.github.com>
2022-09-07 20:33:07 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+class Football extends React.Component {
+  shoot() {
+    alert("Great Shot!");
+  }
+  render() {
+    return (
+      <button onClick={this.shoot}>Take the shot!</button>
+    );
+  }
+}
+
+ReactDOM.render(<Football />, document.getElementById('root'));
+ 
+ 
+ 
+
